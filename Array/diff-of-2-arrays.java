c01d30cdcbafe97a7b919999e45a2f6d1663f067
stupidsud <41414519+stupidsud@users.noreply.github.com>
2021-06-23 02:23:20 +0530

    Update diff-of-2-arrays.java

--- Array/diff-of-2-arrays.java
+++ Array/diff-of-2-arrays.java
@@ -4,18 +4,18 @@ import java.util.*;
 public class Main{
 
 public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Scanner sc = new Scanner(System.in);
 
-    int n1 = Integer.parseInt(br.readLine());
+    int n1 = sc.nextInt();
     int[] a1 = new int[n1];
     for(int i = 0; i < n1; i++){
-       a1[i] = Integer.parseInt(br.readLine());
+       a1[i] = sc.nextInt();
     }
 
-    int n2 = Integer.parseInt(br.readLine());
+    int n2 = sc.nextInt();
     int[] a2 = new int[n2];
     for(int i = 0; i < n2; i++){
-       a2[i] = Integer.parseInt(br.readLine());
+       a2[i] = sc.nextInt();
     }
 
     int[] diff = new int[n2];
